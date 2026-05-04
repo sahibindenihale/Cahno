3a4384b5f8872d1fd0ec866e7852698cd26e2c6e
reisalbay <aviserent@gmail.com>
2026-05-05 01:26:55 +0300

    Update config.js
    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,8 +1,8 @@
 window.SITE_CONFIG = {
   companyName: 'Garent Rent Filo',
-  phoneDisplay: '905054954124',
-  phoneRaw: '905054954124',
-  whatsappRaw: '905054954124',
+  phoneDisplay: '905074638620',
+  phoneRaw: '905074638620',
+  whatsappRaw: '905074638620',
   whatsappMessage: 'Merhaba, Cordinalyus Rent Filo için araç rezervasyonu yapmak istiyorum.',
   workingHours: '7/24 rezervasyon ve iletişim desteği'
 };
